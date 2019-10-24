3882b4082e0ff2a6bd3c5a218c2602aea22eaca3
Articus Pengwin <articuspengwin@gmail.com>
2019-10-25 00:39:10 +1000

    Add favicon

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -10,6 +10,12 @@
     <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
     <link rel="stylesheet" href="{{ mix('css/iconfont.css', 'assets/build') }}">
     <title>{{ $page->siteName }}{{ $page->title ? ' | ' . $page->title : '' }}</title>
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/images/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/assets/images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/assets/images/favicon-16x16.png">
+    <meta name="msapplication-TileColor" content="#394859">
+    <meta name="theme-color" content="#394859">
 </head>
 
 <body id="{{ $page->getSelector() }}">
